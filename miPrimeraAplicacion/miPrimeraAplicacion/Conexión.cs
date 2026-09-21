6e397106cba3d0e13df797131c85ed9dba68424b
joshua alfaro <joshuaalfaro757@gmail.com>
2026-09-21 16:24:36 -0600

    Libreria para usar base de datos

---
+++ miPrimeraAplicacion/miPrimeraAplicacion/Conexión.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;  //Esta libreria es para usar bases de datos
+using System.Data.SqlClient; // Esta libreria permite usar SQLServer
+
+
+namespace miPrimeraAplicacion
+{
+    class Conexión
+    {
+        //Definir los miembro, atributos y metodos de la clase
+        public SqlConnection objConexion = new SqlConnection(); //Conectarme a la BD
+        public SqlCommand objComando = new SqlCommand(); //Ejecutar consultas (Insert, update, delete, select) SQL en la BD
+        public SqlDataAdapter objDataAdapter = new SqlDataAdapter(); //Un puente entre la BD y la aplicacion.
+        DataSet ObjDs = new DataSet(); // Representa una copia en memoria de la arquitectura de la BD
+
+        public Conexión() {// Constructor e inicializadorde los mienbros de la clase
+            String cadenaCoenxion = @"";
+        
+        }
+    }
+}
